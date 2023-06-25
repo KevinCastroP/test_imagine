bda79cf47a52d9659ee70ca033a5aaaeb6c1fd57
KevinCastroP <castrok.c11@hotmail.com>
2023-06-24 19:08:01 -0500

    Create database models

--- test_imagine_apps/logistics/models.py
+++ test_imagine_apps/logistics/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Paquete(models.Model):
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+    dimensiones = models.CharField(max_length=100)
+    direccion_origen = models.CharField(max_length=200)
+    direccion_destino = models.CharField(max_length=200)
+    estado_entrega = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Paquete #{self.id}"
+
+
+class Transportista(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo_vehiculo = models.CharField(max_length=50)
+    contacto = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
